e69fbcd5c227e8fedb16f2ec78fbe88dcb7f8314
JT <jason.s.theobald@gmail.com>
2021-04-19 10:38:51 -0700

    Reorganize project structure

---
+++ client/src/LoginForm.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+import web3 from 'web3'
+import jwt from 'jsonwebtoken'
+
+let CHALLENGE_MSG = 'Connect to GatedCommunity'
+let CONTRACT_ADDRESS = "0x06F166f3D26d13AeB0c55263Ef98211718EB2e4F"
+let AUTH_URL = 'http://127.0.0.1:8080/connect'
+
+class LoginForm extends Component {
+
+  constructor(props) {
+    super(props)
+    this.connect = this.connect.bind(this)
+    this.obtainToken = this.obtainToken.bind(this)
+    this.state = {loggedIn:false}
+  }
+
+  componentDidMount() {
+    this.web3 = new web3(window.ethereum);
+    this.abi = [ { "inputs": [], "stateMutability": "nonpayable", "type": "constructor" }, { "anonymous": false, "inputs": [ { "indexed": true, "internalType": "address", "name": "owner", "type": "address" }, { "indexed": true, "internalType": "address", "name": "approved", "type": "address" }, { "indexed": true, "internalType": "uint256", "name": "tokenId", "type": "uint256" } ], "name": "Approval", "type": "event" }, { "anonymous": false, "inputs": [ { "indexed": true, "internalType": "address", "name": "owner", "type": "address" }, { "indexed": true, "internalType": "address", "name": "operator", "type": "address" }, { "indexed": false, "internalType": "bool", "name": "approved", "type": "bool" } ], "name": "ApprovalForAll", "type": "event" }, { "anonymous": false, "inputs": [ { "indexed": true, "internalType": "address", "name": "from", "type": "address" }, { "indexed": true, "internalType": "address", "name": "to", "type": "address" }, { "indexed": true, "internalType": "uint256", "name": "tokenId", "type": "uint256" } ], "name": "Transfer", "type": "event" }, { "inputs": [ { "internalType": "address", "name": "to", "type": "address" }, { "internalType": "uint256", "name": "tokenId", "type": "uint256" } ], "name": "approve", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [ { "internalType": "address", "name": "owner", "type": "address" } ], "name": "balanceOf", "outputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "stateMutability": "view", "type": "function", "constant": true }, { "inputs": [ { "internalType": "uint256", "name": "tokenId", "type": "uint256" } ], "name": "getApproved", "outputs": [ { "internalType": "address", "name": "", "type": "address" } ], "stateMutability": "view", "type": "function", "constant": true }, { "inputs": [ { "internalType": "address", "name": "owner", "type": "address" }, { "internalType": "address", "name": "operator", "type": "address" } ], "name": "isApprovedForAll", "outputs": [ { "internalType": "bool", "name": "", "type": "bool" } ], "stateMutability": "view", "type": "function", "constant": true }, { "inputs": [], "name": "maxTokens", "outputs": [ { "internalType": "uint8", "name": "", "type": "uint8" } ], "stateMutability": "view", "type": "function", "constant": true }, { "inputs": [], "name": "name", "outputs": [ { "internalType": "string", "name": "", "type": "string" } ], "stateMutability": "view", "type": "function", "constant": true }, { "inputs": [ { "internalType": "uint256", "name": "tokenId", "type": "uint256" } ], "name": "ownerOf", "outputs": [ { "internalType": "address", "name": "", "type": "address" } ], "stateMutability": "view", "type": "function", "constant": true }, { "inputs": [ { "internalType": "address", "name": "from", "type": "address" }, { "internalType": "address", "name": "to", "type": "address" }, { "internalType": "uint256", "name": "tokenId", "type": "uint256" } ], "name": "safeTransferFrom", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [ { "internalType": "address", "name": "from", "type": "address" }, { "internalType": "address", "name": "to", "type": "address" }, { "internalType": "uint256", "name": "tokenId", "type": "uint256" }, { "internalType": "bytes", "name": "_data", "type": "bytes" } ], "name": "safeTransferFrom", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [ { "internalType": "address", "name": "operator", "type": "address" }, { "internalType": "bool", "name": "approved", "type": "bool" } ], "name": "setApprovalForAll", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [ { "internalType": "bytes4", "name": "interfaceId", "type": "bytes4" } ], "name": "supportsInterface", "outputs": [ { "internalType": "bool", "name": "", "type": "bool" } ], "stateMutability": "view", "type": "function", "constant": true }, { "inputs": [], "name": "symbol", "outputs": [ { "internalType": "string", "name": "", "type": "string" } ], "stateMutability": "view", "type": "function", "constant": true }, { "inputs": [], "name": "tokenCount", "outputs": [ { "internalType": "uint8", "name": "", "type": "uint8" } ], "stateMutability": "view", "type": "function", "constant": true }, { "inputs": [ { "internalType": "uint256", "name": "tokenId", "type": "uint256" } ], "name": "tokenURI", "outputs": [ { "internalType": "string", "name": "", "type": "string" } ], "stateMutability": "view", "type": "function", "constant": true }, { "inputs": [ { "internalType": "address", "name": "from", "type": "address" }, { "internalType": "address", "name": "to", "type": "address" }, { "internalType": "uint256", "name": "tokenId", "type": "uint256" } ], "name": "transferFrom", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "obtainToken", "outputs": [], "stateMutability": "nonpayable", "type": "function" } ]
+    this.contract = new this.web3.eth.Contract(this.abi,CONTRACT_ADDRESS)
+  }
+
+  obtainToken(res) {
+    this.contract.methods.obtainToken()
+        .send({ from: window.ethereum.selectedAddress })
+        .then(this.connect)
+        .catch(e=>console.log(e));
+  }
+
+  connect() {
+    try{
+        this.web3.eth.getAccounts()
+            .then(accounts => window.ethereum.request({ method: 'personal_sign', params: [accounts[0], CHALLENGE_MSG] }))
+            .then(signedMsg => axios.post(AUTH_URL, { signedMsg: signedMsg, address: window.ethereum.selectedAddress }))
+            .then(res => this.setState(jwt.decode(res.data.jwt)))
+            .catch((e) => console.log(e))
+    } catch(e) {
+        console.log(e)
+        alert(e)
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        { this.state.address && this.state.authorized ? <div>{this.state.address} is connected and authorized.</div>: null}
+        { this.state.address && !this.state.authorized ? <button onClick={this.obtainToken}>Get Token</button>:null}
+        { !this.state.address ? <div>No address connected.<button onClick={this.connect}>Login</button></div> : null}
+      </div>
+    )
+  }
+}
+
+export default LoginForm
